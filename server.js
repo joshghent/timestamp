85f113ecc7fa12d0bee98087c4031e18bec35183
Josh Ghent <me@joshghent.com>
2016-07-08 00:13:57 +0100

    Added working code. Need to comment and possibly port to ES6!

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+'use strict';
+
 var express = require('express');
 var path = require('path')
-var port = 3005;
 var app = express();
+var port = 3005;
 var http = require('http').Server(app);
 
 // Homepage
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+// API
+app.get('/:input', function(req, res){
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  var input = req.params.input;
+
+  input = /^\d+$/.test(input) ? parseInt(input) * 1000 : input;
+  var date = new Date(input);
+
+  if(isNaN(date) === false && date != null){
+    res.json({
+      "unix"   : Date.parse(date) / 1000,
+      "natural": date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getUTCFullYear()
+    });
+  } else { res.sendStatus(400); }
 });
 
 // Start the server
